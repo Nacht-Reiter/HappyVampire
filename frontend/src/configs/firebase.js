6119a34f2ba66702b1cbb1c5aea7c1cba7f5535e
foobic <foobic@gmail.com>
2019-02-23 18:37:06 +0200

    update

---
+++ frontend/src/configs/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCHYCI7z87FOq4jj7qd1BgSeTFBNnzchuY",
+    authDomain: "hackaton-ddf65.firebaseapp.com",
+    databaseURL: "https://hackaton-ddf65.firebaseio.com",
+    projectId: "hackaton-ddf65",
+    storageBucket: "hackaton-ddf65.appspot.com",
+    messagingSenderId: "699406050139"
+};
