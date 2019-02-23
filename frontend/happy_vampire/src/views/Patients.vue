1f21d533bdecc103d4111134ea3ee7d9cfd69097
Anton Kovalenko <anton.kovalenko.cf@ukr.net>
2019-02-23 07:55:24 -0800

    addded murkup for patients list

--- frontend/happy_vampire/src/views/Patients.vue
+++ frontend/happy_vampire/src/views/Patients.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>patiens</div>
+  <div>
+    <div v-for="patient in patients">
+      <patient :patient="patient" :v-key="patient.key"></patient>
+    </div>
+  </div>
 </template>
 
 <script>
+import patient from "../components/patient.vue";
+const patients = [
+  {
+    name: "roma",
+    letter: "Lettterere erfsfsfefs sfsefse fsefsefsefseffffesfsefsef",
+    bloodType: "2",
+    id: "23"
+  },
+  {
+    name: "roma",
+    letter: "Lettterere erfsfsfefs sfsefse fsefsefsefseffffesfsefsef",
+    bloodType: "2",
+    id: "33"
+  }
+];
 export default {
-  name: "Patiens"
+  name: "Patiens",
+  components: {
+    patient
+  },
+  computed: {
+    patients: () => patients
+  }
 };
 </script>
 
