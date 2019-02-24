175af73d3318952a648bd85c19da7c872631e710
Nacht-Reiter <maxkolpak@ukr.net>
2019-02-24 04:03:59 +0200

    Auth service

---
+++ backend/HappyVampire.BusinessLogic/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using HappyVampire.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyVampire.BusinessLogic.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<DonorDTO> RegisterDonorAsync(string Uid, DonorDTO donor);
+        Task<HospitalDTO> RegisterHospitalAsync(string Uid, HospitalDTO hospital);
+    }
+}
---
+++ backend/HappyVampire/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HappyVampire.BusinessLogic.Interfaces;
+using HappyVampire.Common.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HappyVampire.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService service;
+
+        public AuthController(IAuthService service)
+        {
+            this.service = service;
+        }
+        // POST: Donor/5
+        [HttpPost("donor/{Uid}")]
+        public async Task<IActionResult> RegisterDonor(string Uid, [FromBody] DonorDTO donor)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest() as IActionResult;
+            var info = await service.RegisterDonorAsync(Uid, donor);
+            return info == null ? StatusCode(400) :  Ok(info) as IActionResult;
+        }
+
+        // POST: Hospital/5
+        [HttpPost("hospital/{Uid}")]
+        public async Task<IActionResult> RegisterHospital(string Uid, [FromBody] HospitalDTO hospital)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest() as IActionResult;
+            var info = await service.RegisterHospitalAsync(Uid, hospital);
+            return info == null ? StatusCode(400) : Ok(info) as IActionResult;
+        }
+    }
+
+    
+}
