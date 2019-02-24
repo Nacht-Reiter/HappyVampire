c4a94a3c7cd65abd67aacfb5e76e934f089e546a
foobic <foobic@gmail.com>
2019-02-24 02:55:52 +0200

    Filtration added

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -13,15 +13,18 @@ const removeCookies = () => {
 const addInterceptor = () =>
   axios.interceptors.request.use(
     config => {
-      const token = getTokenFromCookie();
-      if (token) {
+      // const token = getTokenFromCookie();
+      const token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1NjI5OWQ3YTI2ODE0NWJkOWJiMjA2Zjg4ODlkYWMwMjg0NjhmZTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaGFja2F0b24tZGRmNjUiLCJhdWQiOiJoYWNrYXRvbi1kZGY2NSIsImF1dGhfdGltZSI6MTU1MDk2NzQ1OCwidXNlcl9pZCI6IkY5Y0pOR3Q1WVdlTHl1UGFCQ3dOZmJCRWI2cTIiLCJzdWIiOiJGOWNKTkd0NVlXZUx5dVBhQkN3TmZiQkViNnEyIiwiaWF0IjoxNTUwOTY3NDU4LCJleHAiOjE1NTA5NzEwNTgsImVtYWlsIjoiZ3dnZWd3ZWdwQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJnd2dlZ3dlZ3BAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.vUjDB_kEE6sF7ZgRlk3g2u137cOEj4WpbppFFNoUorS3RlYOA-n-UpYnsg_usTyb5FHpAjsgHs6XTAdRseZavkDDfeNs4hNHo8cAXpi9TJTm4aN1jNfitOOKQoxAatA3tqoV9FcvwH3cqA7sedhnK4MVdYB8_wawvDT2dn52sFcCGu3LpzH9vctjr-7M_TWNJij5jjA4_473FmT5kUa6Rd18-7LuxHPBhYhvP86ZEl5furROOYFZJKkW6u1a6PeCa5GeybEXuSNj9-LV23EUMydLC22Kj3taNweQQMHXM8I0gMtvrKURoB_EWBKTYbyZX6s9lfQ2lH5ybhHAKh3zsQ";
+      console.log(token)
+      
+      if (!token) {
         return config;
       }
       const newConfig = {
         headers: {},
         ...config
       };
-
+      console.log(token)
       newConfig.headers.Authorization = `Bearer ${token}`;
       return newConfig;
     },
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -9,11 +9,26 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    authenticated: false,
-    // authenticated: true,
+    // authenticated: false,
+    authenticated: true,
     accountType: "donor",
-    accountType: "hospital",
-    patientList: []
+    // accountType: "hospital",
+    hospital: {
+      id: 1 
+    },
+    donor: {
+      id: 1,
+
+    },
+    patient: {
+      id: null,
+      bloodType: 'I+'
+    },
+    patientList: [],
+    server: {
+      ip: "192.168.32.77",
+      port: "3000"
+    }
   },
   getters,
   mutations,
